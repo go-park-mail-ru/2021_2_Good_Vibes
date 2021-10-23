69e3c2ed459b528791cd4baa8aa997906aa1a817
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2021-10-23 21:33:45 +0300

    product logger

--- internal/app/product/delivery/http/handler.go
+++ internal/app/product/delivery/http/handler.go
@@ -2,7 +2,9 @@ package http
 
 import (
 	"github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/errors"
+	middlewareLogger "github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/middleware/logger"
 	"github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/product"
+	"github.com/sirupsen/logrus"
 
 	"github.com/labstack/echo/v4"
 	"net/http"
@@ -19,6 +21,7 @@ func NewProductHandler(usecase product.Usecase) *ProductHandler {
 	}
 }
 
+const trace = "ProductHandler"
 //это должно быть, пока не нужно
 /*
 func addProduct(ctx echo.Context) error {
@@ -27,8 +30,12 @@ func addProduct(ctx echo.Context) error {
 
 //пока решаем вопросы с пагинацией - так
 func (ph *ProductHandler) GetAllProducts(ctx echo.Context) error {
+	logger := ctx.Get(middlewareLogger.LoggerFieldName).(*logrus.Entry)
+	logger.Trace(trace + "GetAllProducts")
+
 	answer, err := ph.usecase.GetAllProducts()
 	if err != nil {
+		logger.Error(err)
 		newProductError := errors.NewError(errors.DB_ERROR, err.Error())
 		return ctx.JSON(http.StatusBadRequest, newProductError)
 	}
@@ -36,23 +43,28 @@ func (ph *ProductHandler) GetAllProducts(ctx echo.Context) error {
 }
 
 func (ph *ProductHandler) GetProductById(ctx echo.Context) error {
+	logger := ctx.Get(middlewareLogger.LoggerFieldName).(*logrus.Entry)
+	logger.Trace(trace + "GetProductById")
+
 	val := ctx.QueryParams()
 
 	idString := val.Get("id")
 	if idString == "" {
+		logger.Error("bad query param for GetProductById")
 		newProductError := errors.NewError(errors.VALIDATION_ERROR, errors.VALIDATION_DESCR)
 		return ctx.JSON(http.StatusBadRequest, newProductError)
 	}
 
 	id, err := strconv.Atoi(idString)
 	if err != nil {
+		logger.Error(err)
 		newProductError := errors.NewError(errors.SERVER_ERROR, err.Error())
 		return ctx.JSON(http.StatusBadRequest, newProductError)
 	}
 
 	answer, err := ph.usecase.GetProductById(id)
-
 	if err != nil {
+		logger.Error(err)
 		newProductError := errors.NewError(errors.DB_ERROR, err.Error())
 		return ctx.JSON(http.StatusBadRequest, newProductError)
 	}
