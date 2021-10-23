cdc588e1048c8ebf64684ab19f23c16ec677b796
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2021-10-23 21:56:02 +0300

    logger category basket

--- internal/app/basket/delivery/http/handler.go
+++ internal/app/basket/delivery/http/handler.go
@@ -4,8 +4,10 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/basket"
 	"github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/errors"
+	middlewareLogger "github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/middleware/logger"
 	"github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/models"
 	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 )
@@ -20,7 +22,12 @@ func NewBasketHandler(useCase basket.UseCase) *BasketHandler {
 	}
 }
 
+const trace = "BasketHandler"
+
 func (bh *BasketHandler) PutInBasket(ctx echo.Context) error {
+	logger := ctx.Get(middlewareLogger.LoggerFieldName).(*logrus.Entry)
+	logger.Trace(trace + ".PutInBasket")
+
 	var newProduct models.BasketProduct
 	token := ctx.Get("token").(*jwt.Token)
 	claims := token.Claims.(jwt.MapClaims)
@@ -30,29 +37,37 @@ func (bh *BasketHandler) PutInBasket(ctx echo.Context) error {
 
 	newProduct.UserId = int(idNum)
 	if err != nil {
+		logger.Error(err, newProduct.UserId)
 		return ctx.JSON(http.StatusUnauthorized, errors.NewError(errors.TOKEN_ERROR, errors.TOKEN_ERROR_DESCR))
 	}
 
 	if err := ctx.Bind(&newProduct); err != nil {
+		logger.Error(err)
 		newBasketError := errors.NewError(errors.BIND_ERROR, errors.BIND_DESCR)
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
 	if err := ctx.Validate(&newProduct); err != nil {
+		logger.Error(err, newProduct)
 		newBasketError := errors.NewError(errors.VALIDATION_ERROR, errors.VALIDATION_DESCR)
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
 	err = bh.useCase.PutInBasket(newProduct)
 	if err != nil {
+		logger.Error(err, newProduct)
 		newBasketError := errors.NewError(errors.SERVER_ERROR, err.Error())
 		return ctx.JSON(http.StatusInternalServerError, newBasketError)
 	}
 
+	logger.Trace(trace + " success PutInBasket")
 	return ctx.JSON(http.StatusOK, newProduct)
 }
 
 func (bh *BasketHandler) GetBasket(ctx echo.Context) error {
+	logger := ctx.Get(middlewareLogger.LoggerFieldName).(*logrus.Entry)
+	logger.Trace(trace + ".GetBasket")
+
 	var user models.UserID
 
 	token := ctx.Get("token").(*jwt.Token)
@@ -63,6 +78,7 @@ func (bh *BasketHandler) GetBasket(ctx echo.Context) error {
 
 	user.UserId = int(userId)
 	if err != nil {
+		logger.Error(err)
 		return ctx.JSON(http.StatusUnauthorized, errors.NewError(errors.TOKEN_ERROR, errors.TOKEN_ERROR_DESCR))
 	}
 
@@ -70,14 +86,19 @@ func (bh *BasketHandler) GetBasket(ctx echo.Context) error {
 
 	basketProducts, err = bh.useCase.GetBasket(user.UserId)
 	if err != nil {
+		logger.Error(err)
 		newBasketError := errors.NewError(errors.SERVER_ERROR, err.Error())
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
+	logger.Trace(trace + " success GetBasket")
 	return ctx.JSON(http.StatusOK, basketProducts)
 }
 
 func (bh *BasketHandler) DropBasket(ctx echo.Context) error {
+	logger := ctx.Get(middlewareLogger.LoggerFieldName).(*logrus.Entry)
+	logger.Trace(trace + ".DropBasket")
+
 	var user models.UserID
 
 	token := ctx.Get("token").(*jwt.Token)
@@ -85,32 +106,39 @@ func (bh *BasketHandler) DropBasket(ctx echo.Context) error {
 	stringId := claims["id"].(string)
 
 	userId, err := strconv.ParseInt(stringId, 10, 64)
-
 	user.UserId = int(userId)
 	if err != nil {
+		logger.Error(err)
 		return ctx.JSON(http.StatusUnauthorized, errors.NewError(errors.TOKEN_ERROR, errors.TOKEN_ERROR_DESCR))
 	}
 
 	if err := ctx.Bind(&user); err != nil {
+		logger.Error(err)
 		newBasketError := errors.NewError(errors.BIND_ERROR, errors.BIND_DESCR)
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
 	if err := ctx.Validate(&user); err != nil {
+		logger.Error(err, user)
 		newBasketError := errors.NewError(errors.VALIDATION_ERROR, errors.VALIDATION_DESCR)
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
 	err = bh.useCase.DropBasket(user.UserId)
 	if err != nil {
+		logger.Error(err, user)
 		newBasketError := errors.NewError(errors.SERVER_ERROR, err.Error())
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
+	logger.Trace(trace + " success DropBasket")
 	return ctx.JSON(http.StatusOK, user)
 }
 
 func (bh *BasketHandler) DeleteProduct(ctx echo.Context) error {
+	logger := ctx.Get(middlewareLogger.LoggerFieldName).(*logrus.Entry)
+	logger.Trace(trace + ".DeleteProduct")
+
 	var product models.BasketProduct
 
 	token := ctx.Get("token").(*jwt.Token)
@@ -121,24 +149,29 @@ func (bh *BasketHandler) DeleteProduct(ctx echo.Context) error {
 
 	product.UserId = int(userId)
 	if err != nil {
+		logger.Error(err)
 		return ctx.JSON(http.StatusUnauthorized, errors.NewError(errors.TOKEN_ERROR, errors.TOKEN_ERROR_DESCR))
 	}
 
 	if err := ctx.Bind(&product); err != nil {
+		logger.Error(err)
 		newBasketError := errors.NewError(errors.BIND_ERROR, errors.BIND_DESCR)
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
 	if err := ctx.Validate(&product); err != nil {
+		logger.Error(err, product)
 		newBasketError := errors.NewError(errors.VALIDATION_ERROR, errors.VALIDATION_DESCR)
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
 	err = bh.useCase.DeleteProduct(product)
 	if err != nil {
+		logger.Error(err, product)
 		newBasketError := errors.NewError(errors.SERVER_ERROR, err.Error())
 		return ctx.JSON(http.StatusBadRequest, newBasketError)
 	}
 
+	logger.Trace(trace + " success DeleteProduct")
 	return ctx.JSON(http.StatusOK, product)
 }
--- internal/app/category/delivery/http/handler.go
+++ internal/app/category/delivery/http/handler.go
@@ -3,8 +3,10 @@ package http
 import (
 	"github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/category"
 	"github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/errors"
+	middlewareLogger "github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/middleware/logger"
 	"github.com/go-park-mail-ru/2021_2_Good_Vibes/internal/app/models"
 	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -20,30 +22,40 @@ func NewCategoryHandler(useCase category.UseCase) *CategoryHandler {
 	}
 }
 
+const trace = "CategoryHandler"
+
 func (ch *CategoryHandler) GetCategories(ctx echo.Context) error {
+	logger := ctx.Get(middlewareLogger.LoggerFieldName).(*logrus.Entry)
+	logger.Trace(trace + " GetCategories")
+
 	val := ctx.QueryParams()
 
 	nameString := val.Get("name")
 	if nameString == "" {
 		if AllCategoriesJson.Name != "" {
+			logger.Debug(AllCategoriesJson)
 			return ctx.JSON(http.StatusOK, AllCategoriesJson)
 		}
 
 		categories, err := ch.useCase.GetAllCategories()
 		if err != nil {
+			logger.Error(err)
 			newCategoryError := errors.NewError(errors.DB_ERROR, err.Error())
 			return ctx.JSON(http.StatusBadRequest, newCategoryError)
 		}
 		AllCategoriesJson = categories
 
+		logger.Debug(AllCategoriesJson)
 		return ctx.JSON(http.StatusOK, categories)
 	}
 
 	products, err := ch.useCase.GetProductsByCategory(nameString)
 	if err != nil {
+		logger.Error(err)
 		newCategoryError := errors.NewError(errors.SERVER_ERROR, err.Error())
 		return ctx.JSON(http.StatusBadRequest, newCategoryError)
 	}
 
+	logger.Debug(products)
 	return ctx.JSON(http.StatusOK, products)
 }
